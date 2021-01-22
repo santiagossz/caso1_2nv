b264c10864304175b23556a0e066e99e03408708
Santiago <santiago@santiago.local>
2021-01-22 14:29:33 -0500

    cambios realizados

--- src/components/Pokeball.js
+++ src/components/Pokeball.js
@@ -1,27 +1,42 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import CircleType from "circletype";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import List from "./List";
 
 const Pokeball = () => {
 	const textoCircular = useRef(null);
-	const history = useHistory();
+	// const history = useHistory();
+
+	const [background, setBackground] = useState("pokeball");
+	const [open, setOpen] = useState("open");
 
 	useEffect(() => {
-		document.body.className = "pokeball";
+		document.body.className = background;
 		new CircleType(textoCircular.current).radius(270);
 	});
-	function handleClick() {
-		history.push(List);
-	}
+
+	// function handleClick() {
+	// 	history.push(List);
+	// }
 	return (
 		<div>
 			<h1 ref={textoCircular} className='title'>
 				Lista de Pokémon
 			</h1>
-			{/* <button type='button' onClick={handleClick}>
-				Go home
-			</button> */}
+			<button
+				className={open}
+				style={{ opacity: 0 }}
+				onMouseOver={() => {
+					background == "pokeball"
+						? setBackground("list")
+						: window.location.reload(false);
+					open == "open" ? setOpen("noOpen") : setOpen("open");
+				}}
+			>
+				{""}
+			</button>
+
+			{/* <List /> */}
 		</div>
 	);
 };
