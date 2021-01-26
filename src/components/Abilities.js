60444fdf61d0691b9127a9a47b00833b423cdb84
santiagossz <dr.santiagossz@gmail.com>
2021-01-25 21:20:07 -0500

    cambios realizados

--- src/components/Abilities.js
+++ src/components/Abilities.js
@@ -7,16 +7,28 @@ const Abilities = (props) => {
 		return (
 			<div className='abilities_card'>
 				<img src={data.sprites["front_default"]} />
-				{data.abilities.map((ability, index) => {
-					return (
-						<div>
-							<p>{ability.ability.name}</p>
-							<p>{ability.ability.url}</p>
-							<p>{ability.slot}</p>
-							<p>{ability.is_hidden == true ? "Oculta" : "Visible"}</p>
-						</div>
-					);
-				})}
+				<h3 className='name_abilities'>{data.name}</h3>
+				<table>
+					<tr>
+						<th>Habilidad</th>
+						<th>Url</th>
+						<th>Posiciȯn</th>
+						<th>Tipo</th>
+					</tr>
+					{data.abilities.map((ability, index) => {
+						return (
+							<tr>
+								<th>{ability.ability.name}</th>
+								<th>{ability.ability.url}</th>
+								<th>{ability.slot}</th>
+
+								{/* <p className={`p-${index}`}>
+									{ability.is_hidden == true ? "Oculta" : "Visible"}
+								</p> */}
+							</tr>
+						);
+					})}
+				</table>
 			</div>
 		);
 	});
